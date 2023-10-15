b0eb1da356c65a337e96a823ba68f8566381c8ab
BiaoWang <1513119437@qq.com>
2023-10-15 08:48:54 +0800

    实现串口扫描功能

--- SerialDebugTool_Wpf/MainWindow.xaml.cs
+++ SerialDebugTool_Wpf/MainWindow.xaml.cs
@@ -1,20 +1,8 @@
 ﻿using SerialDebugTool_Wpf.ViewModel;
 using System;
-using System.Collections.Generic;
 using System.IO.Ports;
-using System.Linq;
-using System.Management;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SerialDebugTool_Wpf
 {
@@ -28,13 +16,6 @@ namespace SerialDebugTool_Wpf
             InitializeComponent();
 
             this.DataContext = new MainWindowViewModel();
-
-            //获取所有可用串口的名称
-            string[] portNames = SerialPort.GetPortNames();
-            foreach (string portName in portNames)
-            {
-                this.cbPortNumbers.Items.Add(portName);
-            }
         }
 
         /// <summary>
@@ -49,56 +30,5 @@ namespace SerialDebugTool_Wpf
                 this.DragMove();
             }
         }
-
-        #region 监测串口设备插拔
-        private void Test()
-        {
-            StartMonitoringPortChanges();
-
-            Console.WriteLine("正在监测串口设备的插拔情况。");
-            Console.WriteLine("按任意键停止监测。");
-
-            Console.ReadKey();
-
-            if (watcher != null)
-            {
-                watcher.Stop();
-                watcher.Dispose();
-            }
-        }
-
-        private ManagementEventWatcher? watcher;
-
-        private void StartMonitoringPortChanges()
-        {
-            WqlEventQuery query = new WqlEventQuery("SELECT * FROM Win32_DeviceChangeEvent WHERE EventType = 2 OR EventType = 3");
-
-            watcher = new ManagementEventWatcher(new ManagementScope("root\\cimv2"), query);
-            watcher.EventArrived += PortChangeEventReceived;
-            watcher.Start();
-        }
-
-        private void PortChangeEventReceived(object sender, EventArrivedEventArgs e)
-        {
-            int eventType = int.Parse(e.NewEvent.Properties["EventType"].Value.ToString());
-
-            if (eventType == 2)
-            {
-                Console.WriteLine("串口设备已插入.");
-            }
-            else if (eventType == 3)
-            {
-                Console.WriteLine("串口设备已拔出.");
-            }
-
-            // 你可以在这里执行其他操作，例如刷新串口列表
-            string[] availablePorts = SerialPort.GetPortNames();
-            Console.WriteLine("当前可用的串口:");
-            foreach (string port in availablePorts)
-            {
-                Console.WriteLine(port);
-            }
-        }
-        #endregion
     }
 }
--- SerialDebugTool_Wpf/ViewModel/MainWindowViewModel.cs
+++ SerialDebugTool_Wpf/ViewModel/MainWindowViewModel.cs
@@ -1,29 +1,43 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
+using System.IO.Ports;
 
 namespace SerialDebugTool_Wpf.ViewModel
 {
     public partial class MainWindowViewModel : ObservableObject
     {
-        private int? number;
-        public int? Number
+        /// <summary>
+        /// 当前串口号
+        /// </summary>
+        private string? portNumber;
+        public string? PortNumber
         {
-            get => number;
-            set => SetProperty(ref number, value);
+            get => portNumber;
+            set => SetProperty(ref portNumber, value);
         }
 
-        private RelayCommand? getNumbers;
-        public IRelayCommand GetNumbersCommand => getNumbers ??= new RelayCommand(GetNumber);
-
-        private void GetNumber()
+        /// <summary>
+        /// 串口列表
+        /// </summary>
+        private string[]? portNumLst;
+        public string[]? PortNumLst
         {
+            get => portNumLst;
+            set => SetProperty(ref portNumLst, value);
+        }
 
+        /// <summary>
+        /// 获取串口列表
+        /// </summary>
+        private RelayCommand? getPortNumLst;
+        public IRelayCommand GetPortNumLstCommand => getPortNumLst ??= new RelayCommand(GetPortNumLst);
+
+        private void GetPortNumLst()
+        {
+            //获取所有可用串口的名称
+            string[] portNames = SerialPort.GetPortNames();
+
+            PortNumLst = portNames;
         }
 
         /// <summary>
